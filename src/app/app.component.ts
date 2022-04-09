d51ff1be9873b1f18474cf2497af19cd3d38e833
Justin Diaz <justin.s.diaz@gmail.com>
2022-04-09 02:19:10 -0400

    subscribe to router navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import {
+  Router,
+  NavigationStart,
+  Event as NavigationEvent,
+} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'mogul';
+  event$;
+  constructor(private router: Router) {
+    this.event$ = this.router.events.subscribe((event: NavigationEvent) => {
+      if (event instanceof NavigationStart) {
+        console.log(event.url);
+      }
+    });
+  }
 }
