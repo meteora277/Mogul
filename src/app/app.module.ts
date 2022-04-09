37ce3a7f92e7a46055c80b29093e05ce4544c5f1
Justin Diaz <justin.s.diaz@gmail.com>
2022-04-08 23:56:30 -0400

    create root path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DiscoverComponent } from './discover/discover.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, DiscoverComponent],
+  declarations: [AppComponent, DiscoverComponent, HomeComponent, NavbarComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([{ path: 'discover', component: DiscoverComponent }]),
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'discover', component: DiscoverComponent },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent],
