25a984c04730adf23c46196a298e43843312a2c0
Justin Diaz <justin.s.diaz@gmail.com>
2022-04-08 23:47:21 -0400

    add router / create discover route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DiscoverComponent } from './discover/discover.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DiscoverComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{ path: 'discover', component: DiscoverComponent }]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
