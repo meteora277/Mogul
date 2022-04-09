fc047f7176ac2180f314226ff978487a6372eea9
Justin Diaz <justin.s.diaz@gmail.com>
2022-04-09 00:22:59 -0400

    generate card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { DiscoverComponent } from './discover/discover.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
-  declarations: [AppComponent, DiscoverComponent, HomeComponent, NavbarComponent],
+  declarations: [AppComponent, DiscoverComponent, HomeComponent, NavbarComponent, CardComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
