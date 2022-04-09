b82c87b69c95cbff37c0a08b91a84e88d6a38bbb
Justin Diaz <justin.s.diaz@gmail.com>
2022-04-09 00:49:22 -0400

    add inline styles, add @input properties

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styles: [
+    `
+      section {
+        background-color: #c3b4c6;
+        height: 5rem;
+        border-radius: 1rem;
+        padding: 0.8rem;
+        margin: 0 0.3rem;
+        display: flex;
+        flex-direction: column;
+      }
+      h4,
+      h2 {
+        text-align: center;
+        color: white;
+        margin: 0;
+        font-weight: normal;
+        flex-grow: 2;
+      }
+    `,
+  ],
 })
 export class CardComponent implements OnInit {
+  color: string = 'black';
+  @Input() header: string = '';
+  @Input() body: string = '';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
